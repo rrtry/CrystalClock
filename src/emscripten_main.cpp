21b527a298345acc91ff625e8cfe068b08756b30
= <fedormocalov36@gmail.com>
2025-05-17 20:51:21 +0300

    wasm: remove exported functions

--- src/emscripten_main.cpp
+++ src/emscripten_main.cpp
@@ -1,39 +1,13 @@
 #include <emscripten/emscripten.h>
 #include "clock.h"
 
-extern "C"
-{
-    static bool is_running = false;
-
-    void loop_iter(void)
-    {
-        Loop();
-    }
-
-    void start_loop(void)
-    {
-        if (!is_running)
-        {
-            emscripten_set_main_loop(&loop_iter, 0, 1);
-            is_running = true;
-        }
-    }
-
-    void stop_loop(void)
-    {
-        if (is_running) 
-        {
-            emscripten_cancel_main_loop();
-            is_running = false;
-        }
-    }
-}
+extern "C" void loop_iter(void) { Loop(); }
 
 int main(int argc, char** argv)
 {
     if (!Initialize(argc, argv))
         return 1;
 
-    start_loop();
+    emscripten_set_main_loop(loop_iter, 0, 1);
     return 0;
 }
