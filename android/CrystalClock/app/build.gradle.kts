3c56345dd176c953ef82fc77c69bcf4b5e01fc58
rrtry <fedormocalov36@gmail.com>
2025-06-24 18:49:14 +0300

    android: add release signing config

--- android/CrystalClock/app/build.gradle.kts
+++ android/CrystalClock/app/build.gradle.kts
@@ -1,11 +1,27 @@
+import com.android.build.api.variant.FilterConfiguration
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
+val abiCodes = mapOf("armeabi-v7a" to 1, "x86" to 2, "x86_64" to 3, "arm64-v8a" to 4);
+
 android {
     namespace = "com.rrtry.crystalclock"
     compileSdk = 35
+
+    signingConfigs {
+        create("release-cfg") {
+            keyAlias      = "key0"
+            keyPassword   = System.getenv("ANDROID_KEYSTORE_KEY_PASSWORD")
+            storeFile     = file(System.getenv("ANDROID_KEYSTORE_PATH"))
+            storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+    }
 
     defaultConfig {
         applicationId = "com.rrtry.crystalclock"
@@ -21,6 +37,16 @@ android {
                 cppFlags += "-std=c++11"
             }
         }
+        signingConfig = signingConfigs.getByName("release-cfg")
+    }
+
+    splits {
+        abi {
+            this.isEnable = true
+            reset()
+            this.include("x86", "x86_64", "arm64-v8a", "armeabi-v7a")
+            this.isUniversalApk = false
+        }
     }
 
     buildTypes {
@@ -30,7 +56,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release-cfg")
         }
     }
     compileOptions {
@@ -44,6 +70,19 @@ android {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
+        }
+    }
+}
+
+androidComponents {
+    onVariants { variant ->
+        val baseVersionCode = variant.outputs.first().versionCode.get()?.toInt() ?: 1
+        variant.outputs.forEach { output ->
+            val abiFilter = output.filters.find { it.filterType == FilterConfiguration.FilterType.ABI }
+            val baseAbiVersionCode = abiFilter?.identifier?.let { abiCodes[it] }
+            if (baseAbiVersionCode != null) {
+                output.versionCode.set(baseAbiVersionCode * 1000 + baseVersionCode)
+            }
         }
     }
 }
