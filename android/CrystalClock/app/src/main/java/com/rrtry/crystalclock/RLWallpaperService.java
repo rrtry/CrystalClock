f6d9acc752dc859d1baa5019e76acc6adc52c615
rrtry <fedormocalov36@gmail.com>
2025-06-26 18:28:08 +0300

    Update README.MD

--- android/CrystalClock/app/src/main/java/com/rrtry/crystalclock/RLWallpaperService.java
+++ android/CrystalClock/app/src/main/java/com/rrtry/crystalclock/RLWallpaperService.java
@@ -106,6 +106,13 @@ public class RLWallpaperService extends WallpaperService {
             renderHandler.sendEmptyMessage(MSG_INIT);
         }
 
+        // TODO: implement
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            super.onSurfaceChanged(holder, format, width, height);
+            if (isPreview()) return;
+        }
+
         @Override
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
