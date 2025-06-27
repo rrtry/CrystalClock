05382467a8570043f30975ab8c91edb7af3ef3ab
rrtry <fedormocalov36@gmail.com>
2025-06-27 23:37:40 +0300

    android: match application version with latest release tag

--- android/CrystalClock/app/build.gradle.kts
+++ android/CrystalClock/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.rrtry.crystalclock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         externalNativeBuild {
             cmake {
