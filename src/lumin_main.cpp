02b22c8776652b808dfdbb959b50589b44bf40ae
rrtry <fedormocalov36@gmail.com>
2025-08-31 20:27:32 +0300

    Wallpaper: integrate Lumin through CMake

--- src/lumin_main.cpp
+++ src/lumin_main.cpp
@@ -1,6 +1,6 @@
-#include "RaylibDesktop.h"
 #include "raylib.h"
-#include "../clock.h"
+#include "lumin.h"
+#include "clock.h"
 
 extern int GetTargetDisplay();
 
@@ -9,15 +9,15 @@ int main(int argc, char** argv)
 	// Parse config
 	if (!ParseConfig(argc, argv, true)) // prefsOnly: Parse only preference flags (-nosound -nofadein)
 		return 1;
-
+	
 	// Initializes desktop replacement magic
-	InitRaylibDesktop();
+	lumin::Initialize();
 
 	// Sets up the desktop (-1 is the entire desktop spanning all monitors)
-	MonitorInfo monitorInfo = GetWallpaperTarget(GetTargetDisplay());
+	lumin::MonitorInfo monitorInfo = lumin::GetWallpaperTarget(GetTargetDisplay());
 
 	// Set resolution
-	SetWindowResolution(monitorInfo.rcWorkWidth, monitorInfo.rcWorkHeight);
+	SetWindowResolution(monitorInfo.width, monitorInfo.height);
 
 	// Initialize
 	SetShowTime(false);
@@ -27,16 +27,22 @@ int main(int argc, char** argv)
 	void *raylibWindowHandle = GetWindowHandle();
 
 	// Reparent the raylib window to the window behind the desktop icons.
-	RaylibDesktopReparentWindow(raylibWindowHandle);
+	lumin::ConfigureWallpaperWindow(raylibWindowHandle, monitorInfo);
 
-	// Configure the desktop positioning.
-	ConfigureDesktopPositioning(monitorInfo);
+	// Now, enter the raylib render loop.
+	SetTargetFPS(60);
 
 	// Main render loop.
 	while (!WindowShouldClose()) 
 	{
+
 		// skip rendering if the wallpaper is occluded more than 95%
-		if (IsMonitorOccluded(monitorInfo, 0.95) || IsDesktopLocked()) 
+		if (lumin::IsMonitorOccluded(monitorInfo, 0.95)) 
+		{
+			WaitTime(0.1);
+			continue;
+		}
+		if (lumin::IsDesktopLocked() ) 
 		{
 			WaitTime(0.1);
 			continue;
@@ -45,8 +51,6 @@ int main(int argc, char** argv)
 	}
 
 	Uninitialize();
-	// Clean up the desktop window.
-	// Restores the original wallpaper.
-	CleanupRaylibDesktop();
+	lumin::Cleanup();
 	return 0;
 }
